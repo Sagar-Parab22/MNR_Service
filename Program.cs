2efe939584ac687aa51a175a9d4268ed891cd5a7
Sagar-Parab22 <122039453+Sagar-Parab22@users.noreply.github.com>
2025-11-17 10:12:31 +0530

    window Service

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmailService
+namespace WindowsServiceMNR
 {
     static class Program
     {
@@ -14,37 +14,12 @@ namespace EmailService
         /// </summary>
         static void Main()
         {
-#if DEBUG
-            // --- This code runs ONLY in Debug mode ---
-            EmailSendingService myService = new EmailSendingService();
-            try
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
             {
-                // Call the public helper method to start the service's logic
-                myService.StartService();
-
-                // Keep the console window open to let the service run
-                Console.WriteLine("Service is running in debug mode. Press any key to stop...");
-                Console.ReadKey();
-
-                // Call the public helper method to stop the service's logic
-                myService.StopService();
-            }
-            catch (Exception ex)
-            {
-                // This will catch any error that was crashing your app!
-                Console.WriteLine("A fatal error occurred: " + ex.ToString());
-                Console.WriteLine("Press any key to exit.");
-                Console.ReadKey();
-            }
-#else
-    // --- This is the original code for the installed service ---
-    ServiceBase[] ServicesToRun;
-    ServicesToRun = new ServiceBase[]
-    {
-        new EmailSendingService()
-    };
-    ServiceBase.Run(ServicesToRun);
-#endif
+                new WindowService()
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
